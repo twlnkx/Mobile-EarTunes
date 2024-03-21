8445d002ba8d45f65c21fc1ddb2a5998cfeab74a
twlnkx <twinkle.ascano@tup.edu.ph>
2024-03-21 23:05:21 +0800

    not a function error solved

--- backend/app.js
+++ backend/app.js
@@ -2,56 +2,28 @@ const  express = require('express');
 const app = express();
 const morgan = require('morgan');
 const mongoose = require('mongoose');
+const cors = require('cors');
 
 require('dotenv/config');
-
-const  api =  process.env.API_URL;
+app.use(cors());
+app.options('*',cors());
 
 //middleware
 app.use(express.json()); //makes the data be understood sent by the frontend, to the backend
 app.use(morgan('tiny'));
 
 
-const productSchema = mongoose.Schema({
-    name:String, 
-    image:String,
-    desc:String,
-    countInStock:{
-        type:Number,
-        required: true
-    },
-})
+//Routes
+const categoriesRoutes = require('./routes/categories');
+const productsRoutes = require('./routes/products');
+const usersRoutes = require('./routes/users');
+const ordersRoutes = require('./routes/orders');
 
-const Product = mongoose.model('Products',productSchema);
-
-
-app.get(`${api}/products`, async (req, res)=>{
-    const productList = await Product.find();
-
-    if(!productList){
-        res.status(500).json({success: false});
-    }
-
-    res.send(productList);
-})
-
-app.post(`${api}/products`, (req, res)=>{
-    const product = new Product({
-        name: req.body.name,
-        image: req.body.image,
-        desc: req.body.desc,
-        countInStock: req.body.countInStock
-    });
-
-    product.save().then((createdProduct=>{
-        res.status(201).json(createdProduct)
-    })).catch((err)=>{
-        res.status(500).json({
-            error:err,
-            success:false
-        })  
-    })   
-})
+const  api =  process.env.API_URL;
+app.use(`${api}/categories`, categoriesRoutes);
+app.use(`${api}/products`, productsRoutes);
+app.use(`${api}/users`, usersRoutes);
+app.use(`${api}/orders`, ordersRoutes);
 
 mongoose.connect(process.env.CONNECTION_STRING, {
     useNewUrlParser: true,
---
+++ backend/routes/products.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const router = express.Router();
+const {Product} = require('../models/product');
+
+router.get(`/`, async (req, res) => {
+
+    const productList = await Product.find();
+    console.log(Product);
+    if (!productList) {
+        res.status(500).json({ success: false });
+    }
+    res.send(productList);
+});
+
+router.post(`/count`, (req, res)=>{
+    const product = new Product({
+        name: req.body.name,
+        image: req.body.image,
+        desc: req.body.desc,
+        countInStock: req.body.countInStock
+    });
+
+    product.save().then((createdProduct=>{
+        res.status(201).json(createdProduct)
+    })).catch((err)=>{
+        res.status(500).json({
+            error:err,
+            success:false
+        })  
+    })   
+})
+
+module.exports = router;
