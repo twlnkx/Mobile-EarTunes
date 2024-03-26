10b8fe8f7aa610b7ac52d4a19d34b5f4bab1c414
twlnkx <twinkle.ascano@tup.edu.ph>
2024-03-26 23:48:09 +0800

    multiple file upload&update category

--- backend/routes/categories.js
+++ backend/routes/categories.js
@@ -2,6 +2,7 @@ const {Category} = require('../models/category');
 const express = require('express');
 const router = express.Router();
 const multer = require('multer');
+const mongoose = require('mongoose');
 
 const FILE_TYPE_MAP = {
     'image/png': 'png',
@@ -75,13 +76,29 @@ router.post(`/`, uploadOptions.array('image',10),async (req,res)=>{
     res.send(category);
 })
 
-router.put('/:id',async (req, res)=> {
+router.put('/:id',uploadOptions.array('image',10),async (req, res)=> {
+    if (!mongoose.isValidObjectId(req.params.id)) {
+        return res.status(400).send('Invalid Product Id');
+    }
+
+    let imagePath = [];
+    const basePath = `${req.protocol}://${req.get('host')}/public/uploads/`;
+    const files = req.files;
+    if (files && files.length > 0) {
+        files.forEach(file => {
+            imagePath.push(`${basePath}${file.filename}`);
+        });
+    } else {
+        imagePath = product.images;
+    }
+
     const category = await Category.findByIdAndUpdate(
         req.params.id,
             {
                 name: req.body.name,
                 icon: req.body.icon || category.icon,
                 color: req.body.color,
+                image: imagePath
             },
                 { new: true}
             )
